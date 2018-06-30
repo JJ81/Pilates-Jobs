9a40ed3b4d423c2291752252a8438e29c5cdd9e3
yijaejun <yijaejun81@gmail.com>
2018-06-30 20:55:33 +0900

    no message

---
+++ agreement.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="ko">
+<head>
+
+<?php require_once('./inc/head.php') ;?>
+    
+<title>Yoga</title>
+</head>
+
+<body>
+
+<div class="loader"></div>
+
+<!--cookie-->
+<!-- <div class="cookie">
+        <div class="container">
+          <div class="clearfix">
+            <span>Please note this website requires cookies in order to function correctly, they do not store any specific information about you personally.</span>
+            <div class="f-right"><a href="#" class="button button-type-3 button-orange">Accept Cookies</a><a href="#" class="button button-type-3 button-grey-light">Read More</a></div>
+          </div>
+        </div>
+      </div>-->
+
+<!-- - - - - - - - - - - - - - Wrapper - - - - - - - - - - - - - - - - -->
+<div id="wrapper" class="wrapper-container">
+    <!-- - - - - - - - - - - - - Mobile Menu - - - - - - - - - - - - - - -->
+    <nav id="mobile-advanced" class="mobile-advanced"></nav>
+
+    <?php require_once ('./inc/header.php');?>
+
+    <!-- - - - - - - - - - - - - - Breadcrumbs - - - - - - - - - - - - - - - - -->
+    <div class="breadcrumbs-wrap">
+        <div class="container">
+            <h1 class="page-title">이용약관</h1>
+            <ul class="breadcrumbs">
+                <li><a href="index.html">Home</a></li>
+                <li>이용약관</li>
+                <li>Agreement</li>
+            </ul>
+        </div>
+    </div>
+    <!-- - - - - - - - - - - - - end Breadcrumbs - - - - - - - - - - - - - - - -->
+
+    <!-- - - - - - - - - - - - - - Content - - - - - - - - - - - - - - - - -->
+    <div id="content" class="page-content-wrap">
+        <div class="container">
+            <div class="row">
+                <main id="main" class="col-lg-12 col-md-12">
+                    <div class="agreement-area">
+                        이용약관
+                    </div>
+                </main>
+            </div> <!-- // end row -->
+        </div> <!-- // end container -->
+    </div> <!-- // end content -->
+    <!-- - - - - - - - - - - - - end Content - - - - - - - - - - - - - - - -->
+
+    <?php require_once ('./inc/footer.php');?>
+    <?php require_once ('./popup/sign.php');?>
+    <?php require_once ('./popup/login.php');?>
+</div>
+
+<!-- - - - - - - - - - - - end Wrapper - - - - - - - - - - - - - - -->
+<?php require_once ('./inc/tail.php');?>
+
+</body>
+</html>
