02230d47034351db72ad3bd026155af5b15b1f09
yijaejun <yijaejun81@gmail.com>
2018-07-03 21:53:09 +0900

    no message

--- mypage.php
+++ mypage.php
@@ -92,78 +92,79 @@ $db=null;
                                     </div>
 
                                 </div>
-                                <?php }else if($_SESSION['user'] == 'C'){ ?>
+                                <?php }else if($_SESSION['role'] == 'C'){ ?>
                                     <div style="text-align: right;margin-bottom: 10px;">
                                         <a href="#" class="btn btn-primary">정보수정</a>
                                     </div>
+
                                     <?php for($i=0,$size=count($row);$i<$size;$i++) { ?>
-                                        <table class="table table-client-info">
-                                            <colgroup>
-                                                <col width="30%" />
-                                                <col width="70%" />
-                                            </colgroup>
-                                            <tr>
-                                                <td colspan="2" class="center section-title">개인 정보</td>
-                                            </tr>
-                                            <tr>
-                                                <th>
-                                                    이름(실명)
-                                                </th>
-                                                <td><?php echo $row[$i]['realname'] ?></td>
-                                            </tr>
-                                            <tr>
-                                                <th>닉네임</th>
-                                                <td><?php echo $row[$i]['nickname'] ?></td>
-                                            </tr>
-                                            <tr>
-                                                <th>대표사진(프로필)</th>
-                                                <td>
-                                                    <img src="https://www.catholic.edu/assets/images/default_profile.jpg" alt="" width="100" />
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <th>이메일</th>
-                                                <td>
-                                                    <?php echo $row[$i]['email']; ?>
-                                                    <a href="#" class="btn">이메일 인증</a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <th>가입유형</th>
-                                                <td>
-                                                    <?php if($row[$i]['reg_type'] == 'B'){ ?>
-                                                        기업회원
-                                                    <?php }else{ ?>
-                                                        개인회원
-                                                    <?php } ?>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <th>비밀번호</th>
-                                                <td>
-                                                    **********
-                                                    <a href="#" class="btn btn-link">비밀번호 수정</a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <th>연락처</th>
-                                                <td>
-                                                    <?php echo $row[$i]['phone']; ?>
-                                                    <a href="#" class="btn">본인인증</a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <th>주소</th>
-                                                <td>
-                                                    <?php echo $row[$i]['address'] ?>
-                                                    <a href="#" class="btn">주소변경</a>
-                                                </td>
-                                            </tr>
-                                            <?php if ($row[$i]['reg_type'] == 'B'){ ?>
+                                        <!-- 개인회원인 경우 -->
+                                        <?php if($row[$i]['reg_type'] == "B"){ ?>
+                                            <!-- 기업회원인 경우 -->
+                                            <table class="table table-client-info">
+                                                <colgroup>
+                                                    <col width="30%" />
+                                                    <col width="70%" />
+                                                </colgroup>
                                                 <tr>
-                                                    <td colspan="2" class="center section-title">사업자 정보</td>
+                                                    <td colspan="2" class="center section-title">기업 정보</td>
                                                 </tr>
-                                                <!-- 가입유형이 사업자인 경우 -->
+                                                <tr>
+                                                    <th>
+                                                        상호명
+                                                    </th>
+                                                    <td><?php echo $row[$i]['business_name']; ?></td>
+                                                </tr>
+                                                <tr>
+                                                    <th>상호로고</th>
+                                                    <td>
+                                                        <img src="https://www.catholic.edu/assets/images/default_profile.jpg" alt="" width="100" />
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>이메일</th>
+                                                    <td>
+                                                        <?php echo $row[$i]['email']; ?>
+                                                        <a href="#" class="btn">이메일 인증</a>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>가입유형</th>
+                                                    <td>
+                                                        기업회원
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>비밀번호</th>
+                                                    <td>
+                                                        **********
+                                                        <a href="#" class="btn btn-link">비밀번호 수정</a>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>전화번호</th>
+                                                    <td>
+                                                        <?php echo $row[$i]['phone']; ?>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>사업자번호</th>
+                                                    <td>849-05-00337</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>사업자등록증</th>
+                                                    <td>
+                                                        <img src="" alt="" />
+                                                        <a href="#" class="btn">사업자등록증 변경</a>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>사업장 주소</th>
+                                                    <td>
+                                                        <?php echo $row[$i]['address'] ?>
+                                                        <a href="#" class="btn">주소변경</a>
+                                                    </td>
+                                                </tr>
                                                 <tr>
                                                     <th>상호명</th>
                                                     <td><?php echo $row[$i]['business_name'] ?></td>
@@ -174,18 +175,103 @@ $db=null;
                                                 </tr>
                                                 <tr>
                                                     <th>회사소개</th>
-                                                    <td></td>
+                                                    <td>
+                                                        <div style="white-space: pre-line;"><?php echo $row[$i]['description'] ?></div>
+                                                    </td>
                                                 </tr>
-                                            <?php } ?>
-                                        </table>
+                                                <tr>
+                                                    <th rowspan="2">회사주소</th>
+                                                    <td>
+                                                        <div>서울시 노원구 상계동</div>
+                                                        <a href="#" class="btn">주소지 변경</a>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        <?php }else if($row[$i]['reg_type'] == "P"){ ?>
+                                            <table class="table table-client-info">
+                                                <colgroup>
+                                                    <col width="30%" />
+                                                    <col width="70%" />
+                                                </colgroup>
+                                                <tr>
+                                                    <td colspan="2" class="center section-title">개인 정보</td>
+                                                </tr>
+                                                <tr>
+                                                    <th>
+                                                        이름(실명)
+                                                    </th>
+                                                    <td><?php echo $row[$i]['realname'] ?></td>
+                                                </tr>
+                                                <tr>
+                                                    <th>프로필</th>
+                                                    <td>
+                                                        <img src="https://www.catholic.edu/assets/images/default_profile.jpg" alt="" width="100" />
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>이메일</th>
+                                                    <td>
+                                                        <?php echo $row[$i]['email']; ?>
+                                                        <a href="#" class="btn">이메일 인증</a>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>가입유형</th>
+                                                    <td>
+                                                        <?php if($row[$i]['reg_type'] == 'B'){ ?>
+                                                            기업회원
+                                                        <?php }else{ ?>
+                                                            개인회원
+                                                        <?php } ?>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>비밀번호</th>
+                                                    <td>
+                                                        **********
+                                                        <a href="#" class="btn btn-link">비밀번호 수정</a>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>연락처</th>
+                                                    <td>
+                                                        <?php echo $row[$i]['phone']; ?>
+                                                        <a href="#" class="btn">본인인증</a>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>주소</th>
+                                                    <td>
+                                                        <?php echo $row[$i]['address'] ?>
+                                                        <a href="#" class="btn">주소변경</a>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>구직여부</th>
+                                                    <td>
+                                                        구직중
+                                                        <a href="#" class="btn">변경</a>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <th>이력서 등록</th>
+                                                    <td>
+                                                        <a href="#">이력서</a>
+                                                        <a href="#" class="btn">재업로드</a>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                            <!-- TODO 개인회원은 자신을 잘 나타낼 수 있는 방식으로 개인 홈을 알차고 이쁘게 제공하면 좋을 듯하다!!! -->
+                                            <!-- TODO 이곳에 지원이력도 남길 수 있도록 한다. -->
+                                        <?php } ?>
 
-                                        <table class="table">
-                                            <tr>
-                                                <th>사업장명</th>
-                                                <th>사업장주소</th>
-                                            </tr>
-                                        </table>
-                                        <a href="#" class="btn">사업장 추가</a>
+<!--                                        <table class="table">-->
+<!--                                            <tr>-->
+<!--                                                <th>사업장명</th>-->
+<!--                                                <th>사업장주소</th>-->
+<!--                                            </tr>-->
+<!--                                        </table>-->
+<!--                                        <a href="#" class="btn">사업장 추가</a>-->
                                     <?php } ?>
                                 <?php } ?>
                             </div>
